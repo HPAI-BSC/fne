13d19ca04af6f3001b87147e821fb82feecec408
adriantormos <adriantormos98@gmail.com>
2022-09-30 15:59:53 +0200

    Initial commit

---
+++ test/resources/imagedataset_torch.py
@@ -0,0 +1,42 @@
+from torch.utils.data import Dataset
+
+from torchvision.io import read_image
+from torchvision.transforms import Compose, Resize
+
+
+class TrainImageDataset(Dataset):
+    def __init__(self):
+        self.images = [
+            './resources/flowers17/train/0/image_0006.jpg',
+            './resources/flowers17/train/1/image_0141.jpg',
+            './resources/flowers17/train/2/image_0260.jpg',
+            './resources/flowers17/train/3/image_0500.jpg',
+        ]
+        self.labels = [0, 1, 2, 3]
+        self.transform = Compose([Resize((224, 224))])
+
+    def __len__(self): return 4
+
+    def __getitem__(self, item):
+        image = read_image(self.images[item])
+        label = self.labels[item]
+        return self.transform(image).float(), label
+
+
+class TestImageDataset(Dataset):
+    def __init__(self):
+        self.images = [
+            './resources/flowers17/test/0/image_0006.jpg',
+            './resources/flowers17/test/1/image_0141.jpg',
+            './resources/flowers17/test/2/image_0260.jpg',
+            './resources/flowers17/test/3/image_0500.jpg',
+        ]
+        self.labels = [0, 1, 2, 3]
+        self.transform = Compose([Resize((224, 224))])
+
+    def __len__(self): return 4
+
+    def __getitem__(self, item):
+        image = read_image(self.images[item])
+        label = self.labels[item]
+        return self.transform(image).float(), label
